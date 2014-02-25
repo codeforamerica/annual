8cdca03de44d3b8055e817d92779a95be6501a55
Andy Hull <andyhull3@gmail.com>
2014-02-25 13:58:51 -0500

    adding in the old sidebar

--- js/main.js
+++ js/main.js
@@ -483,8 +483,6 @@ $(function(){
     else{
       $($("div.sidebartitle")[i]).show();
       $($("div.sidebartitle")[i]).addClass("appear");
-      $($("div.sidebartitle")[i]).css("left", "-500px");
-      $($("div.sidebartitle")[i]).animate({left: 30}, 800);
     }
   }, function(el, i){
     $($("div.pagebg")[i]).fadeOut({duration:700});
