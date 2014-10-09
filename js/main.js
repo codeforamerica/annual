5482aed9ba355cefe0995aa36da420fe8c2bee2b
Christoph Kisfeld <spam@b-web.org>
2014-10-09 02:10:02 +0200

    Unnötiges JS entfernt

--- js/main.js
+++ js/main.js
@@ -1,19 +1,4 @@
 $(function(){
-  //shows nav when user hovers over the logo
-  var height = $(window).height(),
-  width = $(window).width();
-
-  var setSize = function(){
-    height = $(window).height();
-    width = $(window).width();
-
-    $(".quote").css({width:width, "min-height":height});
-    $(".story").css({width:width, "min-height":height});
-    $(".pagebg").css({width:width, "min-height":height});
-    $(".scrollout").css({height:height});
-  }
-  $('.quote').show();
-  setSize();
   meetup_url = "https://api.meetup.com/ew/events?callback=?&sign=true&status=upcoming&urlname=codeforde&community_urlname=Munster-DE&page=20&sign=true&key=48406a31956272b2f12c3965547d"
   $.getJSON(meetup_url,function (data) { 
     if(data.results.length > 0) {
