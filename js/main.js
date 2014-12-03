fa76aa3fd079e39b6bbc60bb2ef1054b6bd89e33
Christoph Kisfeld <spam@b-web.org>
2014-12-03 19:37:21 +0100

    Fix Meetup URL

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 $(function(){
-  meetup_url = "https://api.meetup.com/ew/events?callback=?&sign=true&status=upcoming&urlname=codeforde&community_urlname=Munster-DE&page=20&sign=true&key=48406a31956272b2f12c3965547d"
+  meetup_url = "https://api.meetup.com/2/events?callback=?&sign=true&status=upcoming&group_urlname=OK-Lab-Munster&format=json&key=48406a31956272b2f12c3965547d"
   $.getJSON(meetup_url,function (data) { 
     if(data.results.length > 0) {
-      result = data.results[0]
-      title = result.short_description;
+      result = data.results[0];
+      title = result.name;
       description = result.description;
       date = result.time;
-      venue = result.venue_name;
+      venue = result.venue.name;
+      eventUrl = result.event_url;
       if(venue) {
         latLng = [result.lat, result.lon];
       }
@@ -14,7 +15,7 @@ $(function(){
       $('#next-meetup #title').html(title);
       $('#next-meetup #venue').html(venue);
       $('#next-meetup #description').html(description);
-      $('#next-meetup #rvsp').attr('href',result.meetup_url);
+      $('#next-meetup #rvsp').attr('href', eventUrl);
 
     }
   });
