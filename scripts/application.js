f0d39bd8e0e8e5ba78d963e6f0d4da9558c24c64
davidrleonard <dleonard@codeforamerica.org>
2015-03-06 17:39:49 -0800

    First pass -- pulling in live data

--- scripts/application.js
+++ scripts/application.js
@@ -20,12 +20,59 @@ var resize = function() {
   $('#js-main').css('margin-top',offset);
 }
 
-$(document).ready(function(){
-  resize();
-});
+// =====
+// Get the data
+// =====
+
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1UTmofeY8rPZvXdN_CNJXfFgPlexiMmlSs5W8oPhqFko/pubhtml';
+
+function init() {
+  Tabletop.init( { key: public_spreadsheet_url,
+                   callback: showInfo,
+                   simpleSheet: true } );
+}
+
+function showInfo(data,tabletop) {
+  console.log("Success!");
+  var stories = tabletop.sheets('story-cards').elements;
+  console.log(stories);
+  writeHTML(stories,'#js-story-template','stories','#js-main');
+}
+
+// =====
+// Publish it to the DOM
+// =====
+
+var writeHTML = function(data, template, slug, target) {
+  // If the endpoint gave us any data
+  if (data.length > 0) {
+    // Build the template
+    var template = _.template(
+      $(template).html(),
+      { variable: slug }
+    );
+    // Write the template to DOM
+    $(target).html(
+      template(data)
+    );
+    return true;
+  } else {
+    console.log('No data. :(');
+    return false;
+  }
+}
 
 $(window).resize(function(){
   resize();
 });
+
+$(document).ready(function(){
+  // Resize the window on ready
+  resize();
+  // Run tabletop
+  init();
+});
+
+
 
 
