bdab286497c944246d80572f0fce193354c8b9d9
davidrleonard <dleonard@codeforamerica.org>
2015-03-09 12:08:17 -0700

    Create a way to encode our category ids

--- scripts/application.js
+++ scripts/application.js
@@ -21,20 +21,67 @@ var resize = function() {
 }
 
 // =====
+// Transform our data
+// =====
+
+// Encode the category names
+// # 'This Is Cool' 
+// => 'this-is-cool'
+
+function encodeValue(data,category,newCategory) {
+  var newStory = _.map(data, function(item){
+   var newEntry = _.mapObject(item, function(val,key) {
+      if (key == category) {
+        var response = _.escape(val);
+        response = response.split(' ').join('-').toLowerCase();
+        return response;
+      } 
+      else {
+        return val;
+      }
+    });
+    return newEntry;
+  });
+  return newStory;
+}
+
+// =====
 // Get the data
 // =====
 
 var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1UTmofeY8rPZvXdN_CNJXfFgPlexiMmlSs5W8oPhqFko/pubhtml';
+var data;
 
 function init() {
   Tabletop.init( { key: public_spreadsheet_url,
-                   callback: showInfo,
+                   callback: handleData,
                    simpleSheet: true } );
 }
 
-function showInfo(data,tabletop) {
+function handleData(data,tabletop) {
   console.log("Success!");
-  var stories = tabletop.sheets('story-cards').elements;
+
+  var stories;
+  var intros;
+  var categories;
+
+  // Put the data into a global variable
+  data = tabletop.sheets;
+
+  // Pull out the stories and section intros
+  stories = tabletop.sheets('story-cards').elements;
+  intros = tabletop.sheets('intros').elements;
+
+  // For each object, encode characters in the identified 'key' (replace spaces with dashes, encode html characters, return a new key/value)
+  stories = encodeValue(stories,'category','category-id');
+  intros = encodeValue(stories,'category','category-id');
+
+  // Make a list of unique categories
+  categories =  _.chain(stories)
+                    .pluck('category')
+                    .uniq()
+                    .value();
+
   console.log(stories);
   writeHTML(stories,'#js-story-template','stories','#js-main');
 }
