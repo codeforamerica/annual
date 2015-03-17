757900f9811f9f9c11b1d2f9836ad2b046054040
davidrleonard <dleonard@codeforamerica.org>
2015-03-16 21:48:24 -0700

    Save Sheet object in memory, poll to see if it's too old and update if needed

--- app.js
+++ app.js
@@ -4,12 +4,34 @@ var fs = require('fs');
 var _ = require('underscore');
 var cons = require('consolidate');
 var app = express();
-var Report = require('./data/Sheet.json');
+
+var getData = require('./getData.js');
+
+var Report;
+Report = getData();
+
+function checkData(){
+  // The time when we got the data
+  created = new Date(Report['created']);
+  // How many seconds ago we got the data
+  seconds = Math.floor((new Date() - created) / 1000);
+  console.log('Data is ' + seconds + ' seconds old...')
+  if (seconds > 300) {
+    // More than 5 minutes ago
+    console.log('\nData out of date, getting new data...');
+    Report = getData();
+  }
+}
 
 app.engine('html', cons.underscore);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.use(express.static('public'));
+
+app.get('*', function(req, res, next){
+  checkData(new Date());
+  next();
+});
 
 app.get('/', function(req, res){
   res.render('index', {
