120f23c91358bdb8d2c504f5ce48236f31cd0274
davidrleonard <dleonard@codeforamerica.org>
2015-03-18 19:18:30 -0700

    We don't need the header resize right now

--- public/scripts/application.js
+++ public/scripts/application.js
@@ -11,19 +11,19 @@
 // Push the content down when the window changes sizes
 // =====
 
-var resize = function() {
-  // Get the height of the header, make it an integer
-  var offset = parseInt($('#js-header').height());
-  // Give it a unit
-  offset = offset + "px";
-  // Apply it to the content
-  $('#js-main').css('margin-top',offset);
-}
+// var resize = function() {
+//   // Get the height of the header, make it an integer
+//   var offset = parseInt($('#js-header').height());
+//   // Give it a unit
+//   offset = offset + "px";
+//   // Apply it to the content
+//   $('#js-main').css('margin-top',offset);
+// }
 
-$(window).resize(function(){
-  resize();
-});
+// $(window).resize(function(){
+//   resize();
+// });
 
-$(document).ready(function(){
-  resize();
-})
+// $(document).ready(function(){
+//   resize();
+// })
